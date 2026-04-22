926a432f1d3a7d159370b1a465586a8845179f61
Aryan Vaghela <aryanvaghela248@gmail.com>
2026-04-22 13:15:10 +0530

    adding cors

--- Backend/index.js
+++ Backend/index.js
@@ -11,37 +11,18 @@ connectDB();
 const app = express();
 
 // Middleware
-// Update CORS middleware
-// app.use(cors({
-//   origin: ['http://localhost:3000', 'http://localhost:3001',"http://localhost:5174", 'http://localhost:5173', 'https://your-frontend-url.com'],
-//   credentials: true,
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// }));
 
-// app.use(cors({
-//   origin: [
-//     "https://gym-fitness-uvnr.onrender.com",
-//     "https://gym-fitness-uvnr.onrender.com",
-//     "https://your-frontend.onrender.com"
-//   ],
-//   credentials: true
-// }));
-
-// app.use(cors({
-//   origin: [
-//     "http://localhost:5173",
-//     "https://gym-fitness-sv4q.vercel.app"   // 👈 YOUR REAL URL
-//   ],
-//   credentials: true
-// }));
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://gym-fitness-8doj.vercel.app"
+    "https://gym-fitness-8doj.vercel.app",
+    "https://gym-fitness-git-main-aryans-projects-539b3387.vercel.app"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true
 }));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
